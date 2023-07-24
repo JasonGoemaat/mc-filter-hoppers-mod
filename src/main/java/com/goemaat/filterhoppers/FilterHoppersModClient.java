98eaa1949d9eb26734b5da8dd1d1f6c87ce5b393
Jason Goemaat <jason.goemaat@gmail.com>
2023-07-24 13:48:59 -0500

    Initial Commit, builds and runs

---
+++ src/main/java/com/goemaat/filterhoppers/FilterHoppersModClient.java
@@ -0,0 +1,10 @@
+package com.goemaat.filterhoppers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FilterHoppersModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/goemaat/filterhoppers/FilterHoppersModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.goemaat.filterhoppers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FilterHoppersModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
